1d933e6c81dbe9f3336fba58d737b518f7674b7a
Doryan Alexandro Mamani Ticona <doryanamt@gmail.com>
2025-02-19 23:00:34 +0100

    Add project files.

---
+++ BeeyondScreen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeeyondScreen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
