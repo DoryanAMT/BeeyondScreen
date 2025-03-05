c7d87550759f0354a7af5bfdddd571f0e314df12
DoryanAlexandro <doryanamt@gmail.com>
2025-03-05 20:21:37 +0100

    Implementación del log in, un pequeño diseño del menu desplegable en el index de Home

--- BeeyondScreen/Controllers/UsuariosController.cs
+++ BeeyondScreen/Controllers/UsuariosController.cs
@@ -1,6 +1,8 @@
-﻿using BeeyondScreen.Models;
+﻿using BeeyondScreen.Extensions;
+using BeeyondScreen.Models;
 using BeeyondScreen.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging.Signing;
 
 namespace BeeyondScreen.Controllers
 {
@@ -30,8 +32,8 @@ namespace BeeyondScreen.Controllers
         public async Task<IActionResult> Create
             (Usuario usuario)
         {
-            await this.repo.InsertUsuario(usuario.UsuarioId, usuario.Nombre,
-                usuario.Correo ,usuario.ContrasenaHash, usuario.FechaCreacion);
+            //await this.repo.InsertUsuario(usuario.UsuarioId, usuario.Nombre,
+            //    usuario.Correo ,usuario.ContrasenaHash, usuario.FechaCreacion);
             return RedirectToAction("Index");
         }
         public async Task<IActionResult> Edit
@@ -44,15 +46,48 @@ namespace BeeyondScreen.Controllers
         public async Task<IActionResult> Edit
             (Usuario usuario)
         {
-            await this.repo.UpdateUsuario(usuario.UsuarioId, usuario.Nombre,
-               usuario.Correo, usuario.ContrasenaHash, usuario.FechaCreacion);
+            //await this.repo.UpdateUsuario(usuario.UsuarioId, usuario.Nombre,
+            //   usuario.Correo, usuario.ContrasenaHash, usuario.FechaCreacion);
             return RedirectToAction("Index");
         }
         public async Task<IActionResult> Delete
             (int idUsuario)
         {
-            await this.repo.DeleteUsuario(idUsuario);
+            //await this.repo.DeleteUsuario(idUsuario);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register
+            (string nombre, string email, string imagen, string password)
+        {
+            await this.repo.RegisterUserAsync(nombre, email, password, imagen);
+            ViewData["MENSAJE"] = "Usuario registrado correctamente";
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogIn
+            (string email, string password)
+        {
+            Usuario usuario = await this.repo.LoginUserAsync(email, password);
+            if (usuario == null)
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas";
+                HttpContext.Session.SetObject("USUARIOCLIENTE", usuario);
+                return View();
+            }
+            else
+            {
+                return View(usuario);
+            }
         }
     }
 }
--- BeeyondScreen/Repositories/RepositoryUsuario.cs
+++ BeeyondScreen/Repositories/RepositoryUsuario.cs
@@ -1,4 +1,5 @@
 ﻿using BeeyondScreen.Data;
+using BeeyondScreen.Helpers;
 using BeeyondScreen.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
@@ -12,7 +13,18 @@ namespace BeeyondScreen.Repositories
         {
             this.context = context;
         }
-        
+        private async Task<int> GetMaxIdUser()
+        {
+            if (this.context.Usuarios.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return await this.context.Usuarios.MaxAsync
+                    (x => x.IdUsuario) + 1;
+            }
+        }
         public async Task<List<Usuario>> GetUsuariosAsync()
         {
             var consulta = from datos in this.context.Usuarios
@@ -23,41 +35,83 @@ namespace BeeyondScreen.Repositories
             (int idUsuario)
         {
             var consulta = from datos in this.context.Usuarios
-                           where datos.UsuarioId == idUsuario
+                           where datos.IdUsuario == idUsuario
                            select datos;
             return await consulta.FirstOrDefaultAsync();
         }
-        public async Task InsertUsuario
-            (int idUsuario, string nombre, string correo, 
-            string contrasenaHash, DateTime fechaCreacion)
+        //public async Task InsertUsuario
+        //    (int idUsuario, string nombre, string email, 
+        //    string contrasenaHash, DateTime fechaCreacion)
+        //{
+        //    Usuario usuario = new Usuario();
+        //    usuario.IdUsuario = idUsuario;
+        //    usuario.Nombre = nombre;
+        //    usuario.Email = email;
+        //    usuario.Pass = contrasenaHash;
+        //    usuario.FechaCreacion = fechaCreacion;
+        //    await this.context.AddAsync(usuario);
+        //    await this.context.SaveChangesAsync();
+        //}
+        //public async Task UpdateUsuario
+        //    (int idUsuario, string nombre,string correo, 
+        //    string contrasenaHash, DateTime fechaCreacion)
+        //{
+        //    Usuario usuario = await this.FindUsuarioAsync(idUsuario) ;
+        //    usuario.UsuarioId = idUsuario;
+        //    usuario.Nombre = nombre;
+        //    usuario.Correo = correo;
+        //    usuario.ContrasenaHash = contrasenaHash;
+        //    usuario.FechaCreacion = fechaCreacion;
+        //    await this.context.SaveChangesAsync();
+        //}
+        //public async Task DeleteUsuario
+        //    (int idUsuario)
+        //{
+        //    Usuario usuario = await this.FindUsuarioAsync(idUsuario);
+        //    this.context.Remove(usuario);
+        //    await this.context.SaveChangesAsync();
+        //}
+        public async Task RegisterUserAsync
+            (string nombre, string email, string password,
+            string imagen)
         {
             Usuario usuario = new Usuario();
-            usuario.UsuarioId = idUsuario;
+            usuario.IdUsuario = await this.GetMaxIdUser();
             usuario.Nombre = nombre;
-            usuario.Correo = correo;
-            usuario.ContrasenaHash = contrasenaHash;
-            usuario.FechaCreacion = fechaCreacion;
-            await this.context.AddAsync(usuario);
+            usuario.Email = email;
+            usuario.Imagen = imagen;
+            usuario.Salt = HelperCriptography.GenerateSalt();
+            usuario.Pass = HelperCriptography.EncryptPassword(password, usuario.Salt);
+            usuario.FechaCreacion = DateTime.Now;
+            this.context.Usuarios.Add(usuario);
             await this.context.SaveChangesAsync();
         }
-        public async Task UpdateUsuario
-            (int idUsuario, string nombre,string correo, 
-            string contrasenaHash, DateTime fechaCreacion)
+        public async Task<Usuario> LoginUserAsync
+            (string email, string password)
         {
-            Usuario usuario = await this.FindUsuarioAsync(idUsuario) ;
-            usuario.UsuarioId = idUsuario;
-            usuario.Nombre = nombre;
-            usuario.Correo = correo;
-            usuario.ContrasenaHash = contrasenaHash;
-            usuario.FechaCreacion = fechaCreacion;
-            await this.context.SaveChangesAsync();
-        }
-        public async Task DeleteUsuario
-            (int idUsuario)
-        {
-            Usuario usuario = await this.FindUsuarioAsync(idUsuario);
-            this.context.Remove(usuario);
-            await this.context.SaveChangesAsync();
+            var consulta = from datos in this.context.Usuarios
+                           where datos.Email == email
+                           select datos;
+            Usuario usuario = await consulta.FirstOrDefaultAsync();
+            if (usuario == null)
+            {
+                return null;
+            }
+            else
+            {
+                string salt = usuario.Salt;
+                byte[] temp = HelperCriptography.EncryptPassword(password, salt);
+                byte[] passBytes = usuario.Pass;
+                bool response = HelperCriptography.CompararArrays(temp, passBytes);
+                if (response == true)
+                {
+                    return usuario;
+                }
+                else
+                {
+                    return null;
+                }
+            }
         }
     }
 }
